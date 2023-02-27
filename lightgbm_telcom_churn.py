6f62844aeae751e50d6e5a7b212b61f6d03592d8
falken1983 <lluis2001@protonmail.ch>
2023-02-27 19:08:54 +0100

    1. ADD `optuna_runs` folder 2. UPDATE `lightgbm_optuned_handler` with history of optuna runs

--- lightgbm_telcom_churn.py
+++ lightgbm_telcom_churn.py
@@ -5,6 +5,8 @@ In this example, we optimize the validation accuracy of telcom churn using Light
 We optimize both the choice of booster model and their hyperparameters.
 
 """
+import os
+
 import pandas as pd
 import numpy as np
 import optuna
@@ -16,10 +18,19 @@ from sklearn.preprocessing import PowerTransformer, StandardScaler, RobustScaler
 from sklearn.model_selection import train_test_split
 import sklearn.metrics
 
+# optuna logging
+from datetime import datetime
+import joblib
+
+
 # FYI: Objective functions can take additional arguments
 # (https://optuna.readthedocs.io/en/stable/faq.html#objective-func-additional-args).
+THISFILE_DIRNAME = os.path.dirname(os.path.realpath(__file__))
+#CLEAN_DATA_REALPATH = THISFILE_DIRNAME + "/" + "kaggle_data/cleaned_train.csv"
+CLEAN_DATA_REALPATH = THISFILE_DIRNAME + "/" + "datamecum_data/cleaned_telcom_churn_construction.csv"
+OPTUNA_RESULTS_REALPATH = THISFILE_DIRNAME + "/" + "optuna_runs"
 
-def load_cleaned_telcom_churn(path="./cleaned_telcom_churn_construction.csv"):
+def load_cleaned_telcom_churn(path=CLEAN_DATA_REALPATH):
     data_raw = pd.read_csv(path)    
     X = data_raw.iloc[:,:-1]
     y = data_raw[data_raw.columns.tolist()[-1]]
@@ -57,7 +68,27 @@ def objective(trial):
     dtrain = lgb.Dataset(train_x, label=train_y)
 
     param = {
-        "objective": "binary",
+        'objective': 'binary',
+        'metric': 'binary_logloss',
+        'verbosity': -1,
+        'boosting_type': 'gbdt',
+        #"device_type": trial.suggest_categorical("device_type", ['gpu']),
+        #"n_estimators": trial.suggest_int("n_estimators", 10,100, step=10),
+        "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.3),
+        "num_leaves": trial.suggest_int("num_leaves", 20, 3000, step=20),
+        "max_depth": trial.suggest_int("max_depth", 3, 12),
+        "min_data_in_leaf": trial.suggest_int("min_data_in_leaf", 200, 10000, step=100),
+        "max_bin": trial.suggest_int("max_bin", 200, 300),
+        "lambda_l1": trial.suggest_float("lambda_l1", 1e-7, 10, log=True),
+        "lambda_l2": trial.suggest_float("lambda_l2", 1e-7, 10, log=True),
+        "min_gain_to_split": trial.suggest_float("min_gain_to_split", 0, 15),
+        "bagging_fraction": trial.suggest_float("bagging_fraction", 0.2, 0.95, step=0.05),
+        "feature_fraction": trial.suggest_float("feature_fraction", 0.2, 0.95, step=0.05),
+        "bagging_freq": trial.suggest_int("bagging_freq", 1, 7),
+    }
+
+    """ 
+                 "objective": "binary",
         "metric": "binary_logloss",
         "verbosity": -1,
         "boosting_type": "gbdt",
@@ -68,7 +99,8 @@ def objective(trial):
         "bagging_fraction": trial.suggest_float("bagging_fraction", 0.4, 1.0),
         "bagging_freq": trial.suggest_int("bagging_freq", 1, 7),
         "min_child_samples": trial.suggest_int("min_child_samples", 5, 100),
-    }
+
+       """
 
     gbm = lgb.train(param, dtrain)
     preds = gbm.predict(valid_x)
@@ -79,8 +111,8 @@ def objective(trial):
 
 if __name__ == "__main__":
     study = optuna.create_study(direction="maximize")
-    data, target = load_cleaned_telcom_churn("../kaggle_data/cleaned_train.csv")
-    print(f"{path} succesfully loaded and parsed to Pandas objects\n")
+    data, target = load_cleaned_telcom_churn()
+    print(f"{CLEAN_DATA_REALPATH} succesfully loaded and parsed to Pandas objects\n")
 
     study.optimize(objective, n_trials=500)
 
@@ -94,3 +126,8 @@ if __name__ == "__main__":
     print("  Params: ")
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
+
+    # logging
+    the_time_is_now = datetime.now().strftime("%Y%m%d_%H%M")
+    filename_dump = f"{OPTUNA_RESULTS_REALPATH}" + "/" + f"{the_time_is_now}_lgb_optuna_study_batch.pkl" 
+    joblib.dump(study, filename=filename_dump)
